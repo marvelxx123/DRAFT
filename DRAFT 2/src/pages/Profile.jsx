91a17c5fe1a1bbbe79d7e3b5b3ef6e581b8bd5c7
marvelxx123 <142357500+marvelxx123@users.noreply.github.com>
2026-05-02 17:26:13 -0400

    Update Profile.jsx

--- DRAFT 2/src/pages/Profile.jsx
+++ DRAFT 2/src/pages/Profile.jsx
@@ -67,9 +67,9 @@ export default function Profile({ session }) {
     setUplAv(true)
     try {
       const ext  = file.name.split('.').pop()
-      const path = `avatars/${session.user.id}.${ext}`
-      await supabase.storage.from('avatars').upload(path, file, { upsert:true })
-      const { data:{ publicUrl } } = supabase.storage.from('avatars').getPublicUrl(path)
+      const path = `${session.user.id}.${ext}`
+      await supabase.storage.from('videos').upload(path, file, { upsert:true })
+      const { data:{ publicUrl } } = supabase.storage.from('videos').getPublicUrl(path)
       await updateProfile(session.user.id, { avatar_url: publicUrl })
       setProf(p=>({...p, avatar_url:publicUrl}))
     } catch(e) { console.error(e) }
